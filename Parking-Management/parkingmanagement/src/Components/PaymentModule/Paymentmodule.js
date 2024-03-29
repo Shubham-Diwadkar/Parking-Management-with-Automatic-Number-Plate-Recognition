b111479f1e10e69d79d55d63229848e50cf73597
Shubham Diwadkar <125255910+Shubham-Diwadkar@users.noreply.github.com>
2024-03-30 00:14:53 +0530

    Update Paymentmodule.js
    Added necessary comments and removed my API Key so that every one can use the code

--- Parking-Management/parkingmanagement/src/Components/PaymentModule/Paymentmodule.js
+++ Parking-Management/parkingmanagement/src/Components/PaymentModule/Paymentmodule.js
@@ -1,35 +1,25 @@
+// Importing required files for Payment Module Component
 import React, { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
 import './Paymentmodule.css'
 
-// import ParkingForm from "../ParkingForm/Parkingform";
-
 const Paymentmodule = () => {
+  
   const navigate = useNavigate();
   const location = useLocation();
-  // const [data, setData] = useState(null);
   const [paymentInProgress, setPaymentInProgress] = useState(false);
   const [price, setPrice] = useState(null); // State to store the price
-  // const [name, setName] = useState('');
-  // const [email, setEmail] = useState('');
-  // const [phoneNumber, setPhoneNumber] = useState('');
-
+  
   // Parse the URL parameters to retrieve formData.price
   useEffect(() => {
+    
     const searchParams = new URLSearchParams(location.search);
     const price = searchParams.get("price");
-    // const name = searchParams.get('name');
-    // const email = searchParams.get('email');
-    // const phoneNumber = searchParams.get('phoneNumber');
     console.log("Price retrieved from URL parameter: ", price);
-    // console.log("Name retrieved from URL parameter: ", name);
-    // console.log("Email retrieved from URL parameter: ", email);
-    // console.log("PhoneNumber retrieved from URL parameter: ", phoneNumber);
+    
     setPrice(price); // Set the price in state
-    // setName(name || ''); // Set the name in state, with an empty string as a fallback
-    // setEmail(email || ''); // Set the email in state, with an empty string as a fallback
-    // setPhoneNumber(phoneNumber || ''); // Set the phoneNumber in state, with an empty string as a fallback
+    
   }, [location.search]);
 
   const handlePayment = async () => {
@@ -51,22 +41,17 @@ const Paymentmodule = () => {
       console.log("Order ID: ", data.id);
 
       const options = {
-        key: "rzp_test_o2gjG5fQUCPsaf", // Replace with your Razorpay key
+        key: "YOUR_RAZORPAY_KEY_ID", // Replace with your Razorpay key
         amount: data.amount, // The amount in paise (1 Rupee = 100 paise)
         currency: "INR",
         name: "Park-King",
         description: "Payment for Parking",
         order_id: data.id,
         handler: function (response) {
-          alert(response.razorpay_payment_id);
-          alert(response.razorpay_order_id);
-          alert(response.razorpay_signature);
+          alert("Payment Successfull");
           navigate("/Home");
         },
         prefill: {
-          // name: name, // Pre-filled customer name
-          // email: email, // Pre-filled customer email
-          // contact: phoneNumber, // Pre-filled customer contact number
         },
         notes: {
           address: "Razorpay Corporate Office",
